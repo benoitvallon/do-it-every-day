c3790c04dbb95093a9770d44c541aa40beb25b19
Benoit VALLON <benoitvallon@gmail.com>
2015-07-29 17:31:50 -0400

    Adds name field to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729185615) do
+ActiveRecord::Schema.define(version: 20150729212112) do
 
   create_table "goals", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150729185615) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
