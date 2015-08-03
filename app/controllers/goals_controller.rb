e1224e1f297dce83ef99c54cc590d995fbfbf569
Benoit VALLON <benoitvallon@gmail.com>
2015-08-03 17:40:33 -0400

    Adds styles to goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -37,7 +37,7 @@ class GoalsController < ApplicationController
 
     respond_to do |format|
       if @goal.save
-        format.html { redirect_to @goal, notice: 'Goal was successfully created.' }
+        format.html { redirect_to goals_path, notice: 'Goal was successfully created.' }
         format.json { render :show, status: :created, location: @goal }
       else
         format.html { render :new }
@@ -51,7 +51,7 @@ class GoalsController < ApplicationController
   def update
     respond_to do |format|
       if @goal.update(goal_params)
-        format.html { redirect_to @goal, notice: 'Goal was successfully updated.' }
+        format.html { redirect_to goals_path, notice: 'Goal was successfully updated.' }
         format.json { render :show, status: :ok, location: @goal }
       else
         format.html { render :edit }
