15447f038335d292efcb0def975324b3be1465ca
Benoit VALLON <benoitvallon@gmail.com>
2015-08-05 16:49:54 -0400

    Adds model validations

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,13 @@
 class Goal < ActiveRecord::Base
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
+  validates :description, presence: true, length: { maximum: 255 }
+  validates :start_date, presence: true
+  validates :duration, presence: true
+  validates :moment_start, presence: true
+  validates :moment_end, presence: true
+
   has_many :activities, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence: true, length: { maximum: 50 }
 
   has_many :goals, dependent: :destroy
 end
