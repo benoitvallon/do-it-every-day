b61134b1322bc5d3559e69ee2c0fc27e90da5d5b
Benoit VALLON <benoitvallon@gmail.com>
2015-08-07 19:21:00 -0400

    Fixes menu visibility on mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,10 +21,36 @@
 $(window).scroll(function () {
   var heightLimit = 100,
       height = $(window).scrollTop();
+      navbarCollapsed = $('.navbar-collapse.in').length;
 
-  if(height > heightLimit) {
-    $('.navbar').removeClass('navbar-transparent');
-  } else {
-    $('.navbar').addClass('navbar-transparent');
+  if(!navbarCollapsed) {
+    if(height > heightLimit) {
+      $('.navbar').removeClass('navbar-transparent');
+    } else {
+      $('.navbar').addClass('navbar-transparent');
+    }
   }
 });
+
+var ready = function() {
+  $('#bs-example-navbar-collapse-1').on('show.bs.collapse', function () {
+    var heightLimit = 100,
+        height = $(window).scrollTop();
+
+    if(height <= heightLimit) {
+      $('.navbar').removeClass('navbar-transparent');
+    }
+  });
+
+  $('#bs-example-navbar-collapse-1').on('hide.bs.collapse', function () {
+    var heightLimit = 100,
+        height = $(window).scrollTop();
+
+    if(height <= heightLimit) {
+      $('.navbar').addClass('navbar-transparent');
+    }
+  });
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
