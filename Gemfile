9754937e602b8f4c282424633afc0c3376310ae0
Benoit VALLON <benoitvallon@gmail.com>
2015-08-07 09:51:00 -0400

    Adds compression to resources in heroku

--- Gemfile
+++ Gemfile
@@ -58,4 +58,5 @@ end
 group :production do
   gem 'pg'
   gem 'rails_12factor'
+  gem 'heroku-deflater'
 end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module DoItEveryDay
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Add compression to resources in heroku
+    config.middleware.use Rack::Deflater
   end
 end
